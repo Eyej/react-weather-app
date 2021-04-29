d7cc3c647f450ffb40a48dd15e3bf7b6cbae520b
Eyej <Eyej@users.noreply.github.com>
2021-04-29 18:26:12 +0100

    added API integration and logic in case there's an error

--- src/Weather.js
+++ src/Weather.js
@@ -5,54 +5,95 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type any city"
-              autoFocus="on"
-              className="form-control"
-            />
-          </div>
+  const [ready, setReady] = useState(false);
+  let [weatherData, setWeatherData] = useState({});
 
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-success" />
-          </div>
-        </div>
-      </form>
+  function displayWeather(response) {
+    setReady(true);
+    console.log(response.data);
+    setWeatherData({
+      name: response.data.name,
+      temperature: Math.round(response.data.main.temp_max),
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      countrySC: response.data.sys.country,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      // src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+    });
+  }
 
-      <h1>London</h1>
-      <ul>
-        <li>Wednesday 09:00am</li>
-        <li>Sunny</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Weather-logo"
-              className="float-left"
-            />{" "}
-            <div className="float-left">
-              <span className="temperature">12</span>
-              <span className="unit">°C</span>
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type any city"
+                autoFocus="on"
+                className="form-control"
+              />
+            </div>
+
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-success" />
             </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 49%</li>
-            <li>Wind: 35Km/ph</li>
-          </ul>
+        </form>
+
+        <h1>
+          {weatherData.name}, {weatherData.countrySC}
+        </h1>
+        <ul>
+          <li>Wednesday 09:00am</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src={weatherData.icon}
+                alt={weatherData.description}
+                className="float-left"
+              />{" "}
+              <div className="float-left">
+                <span className="temperature">{weatherData.temperature}</span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind}Km/ph</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "386b70f96b3e09e40aefe57eb2e44f5e";
+    let unit = "metric";
+    let city = "Mapperley";
+    let endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
+    axios.get(endpoint).then(displayWeather);
+
+    return (
+      <div>
+        <p>Loading...</p>
+        <Loader
+          type="ThreeDots"
+          color="#00BFFF"
+          height={80}
+          width={80}
+          timeout={30000}
+        />
+      </div>
+    );
+  }
 }
 
 // export default function App() {
@@ -65,28 +106,7 @@ export default function Weather() {
 //     setCity(event.target.value);
 //   }
 
-//   function displayWeather(response) {
-//     setData(true);
-//     setWeather({
-//       name: response.data.name,
-//       temperature: Math.round(response.data.main.temp_max),
-//       description: response.data.weather[0].description,
-//       humidity: response.data.main.humidity,
-//       wind: response.data.wind.speed,
-//       countrySC: response.data.sys.country,
-//       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-//     });
-//     console.log(response.data);
-//   }
-//   // Axios call onsubmit
-//   function getWeather(event) {
-//     event.preventDefault();
-//     let apiKey = "386b70f96b3e09e40aefe57eb2e44f5e";
-//     let unit = "metric";
-//     let endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
-
-//     axios.get(endpoint).then(displayWeather);
-//   }
+//
 //   let form = (
 //     <div className="Weather">
 //       <form onSubmit={getWeather}>
@@ -95,42 +115,4 @@ export default function Weather() {
 //       </form>
 //     </div>
 //   );
-//   // console.log(data);
-//   if (data) {
-//     return (
-//       <div className="Weather">
-//         {/* <ul>
-//           {cities.map(function (el, index) {
-//             return <li key={index}>{el}</li>;
-//           })}
-//         </ul>{" "} */}
-//         {form}
-//         <h3>
-//           {weather.name}, {weather.countrySC}
-//         </h3>
-//         <p>{weather.description}</p>
-//         <p>
-//           <img src={weather.icon} alt="Weather icon" />
-//           <span>{weather.temperature}°C</span>
-//         </p>
-//         <ul>
-//           <li>Humidity: {weather.humidity}%</li>
-//           <li>Wind: {weather.wind}Km/ph</li>
-//         </ul>
-//       </div>
-//     );
-//   } else {
-//     return (
-//       <div className="Weather">
-//         {form}
-//         <Loader
-//           type="ThreeDots"
-//           color="#00BFFF"
-//           height={80}
-//           width={80}
-//           timeout={30000}
-//         />
-//       </div>
-//     );
-//   }
-// }
+//
